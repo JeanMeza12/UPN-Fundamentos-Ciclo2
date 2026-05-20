0abaf6c8e8784d89cd02b8d998f5d6fad595556f
sebasgill <sebasgill>
2026-05-19 21:23:27 -0500

    Mi segundo commit

---
+++ segundo.py
@@ -0,0 +1,3 @@
+print("Hola, UPN!")
+print("Bienvenidos a mi primera clase del curso de Fundamentos de Programación")
+print("Estoy emocionado de aprender Python y mejorar mis habilidades de programación")
